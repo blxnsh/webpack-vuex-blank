b51ce8fdf8b9b7323b8a4774ea3a4d69a67311cb
blxnsh <blxnsh@gmail.com>
2017-11-26 12:57:51 +0200

    Initial commit

---
+++ src/components/main.vue
@@ -0,0 +1,17 @@
+<template>
+  <div id="main">
+    <img src="http://freehdwallpapers.website/wp-content/uploads/2016/07/prefect_winter-2560x1440.jpg"
+    alt="Красота" height="200em">
+  </div>
+</template>
+<script>
+
+export default {
+  name: "main",
+  data: () => ({
+
+  })
+}
+</script>
+<style>
+</style>
